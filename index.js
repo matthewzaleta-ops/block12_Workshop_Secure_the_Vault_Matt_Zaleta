216cc50556598406b22d2045861beb2059d9320e
matthewzaleta-ops <matthew.zaleta@gmail.com>
2025-10-20 15:47:38 -0400

    Completed block12 Workshop Secure the Vault

---
+++ index.js
@@ -0,0 +1,21 @@
+let code1;
+code1 = 3 + 7;
+/*console.log(code1); */
+
+let code2 = 100 / 2 - 10;
+/*console.log(code2); */
+
+const code3 = (18 % 5) * 13;
+/*console.log(code3); */
+
+const message = "The vault has been secured.  The combination is:";
+/*console.log(message);*/
+const codeA = code1 + "-" + code2 + "-" + code3;
+/*console.log(codeA); */
+
+let codeB = `${code1}-${code2}-${code3}`;
+/*console.log(codeB); */
+
+console.log(message);
+console.log(codeA);
+console.log(codeB);
